47655a919a41f2ca29015892a3d1cc54e6b48f92
Dan Lawson <danlawson1994@live.co.uk>
2020-03-21 18:44:37 +0000

    Syntax fixes

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,88 +1,81 @@
 ﻿$(function() {
-	setTimeout(function() {
-		$('.pageLoad').fadeToggle();
-		$(this).scrollTop(0);
-		$('#scrollToTop').fadeOut();
-		$('.darkModeToggle').fadeOut();
-	}, 2000);
+  setTimeout(function() {
+    $('.pageLoad').fadeToggle();
+    $(this).scrollTop(0);
+    $('.scrollToTop').fadeOut();
+  }, 2000);
 
-	var pageTitle = $("title").text();
+  var pageTitle = $("title").text();
 
-	$(window).blur(function() {
-	  	$("title").text("👋");
-	});
+  $(window).blur(function() {
+    $("title").text("👋");
+  });
 
-	$(window).focus(function() {
-	  	$("title").text(pageTitle);
-	});
+  $(window).focus(function() {
+    $("title").text(pageTitle);
+  });
 
-	$(window).scroll(function() {
-		$('nav').toggleClass('scrolled', $(this).scrollTop() > 80);
+  $(window).scroll(function() {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 80);
 
-		if($(this).scrollTop() > 80) {
-			$('#scrollToTop').fadeIn();
-		}
-		else {
-			$('#scrollToTop').fadeOut();
-		}
-	});
+    if ($(this).scrollTop() > 80) {
+      $('.scrollToTop').fadeIn();
+    }
+    else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
 
-	
-
-	$(document).ready(function() {
-		$(this).scrollTop(0);
+  $(document).ready(function() {
+    $(this).scrollTop(0);
 		
-		$("a").on('click', function(event) {
-		  	if (this.hash !== "") {
-				event.preventDefault();
-				var hash = this.hash;
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
 		
-				$('html, body').animate( {
-					scrollTop: $(hash).offset().top
-					}, 1000, function() {
-					window.location.hash = hash;
-				});
-		  	}
-		});
+        $('html, body').animate( {
+          scrollTop: $(hash).offset().top
+        }, 1000, function() {
+          window.location.hash = hash;
+        });
+      }
+    });
 
-		$('#darkMode').click(function() {
-			$('.darkModeToggle').fadeToggle();
-		});
-		
-		$('.menuIcon span').removeClass('open');
-		$('.navbar-collapse').collapse('hide');
+    $('.menuIcon span').removeClass('open');
+    $('.navbar-collapse').collapse('hide');
 
-		$('.navLogo').click(function() {
-			if ($('.menuIcon').hasClass('open')) {
-				$('.menuIcon').removeClass('open');
-				$('.navbar-collapse').collapse('hide');
-			}
-		});
+    $('.navLogo').click(function() {
+      if ($('.menuIcon').hasClass('open')) {
+        $('.menuIcon').removeClass('open');
+        $('.navbar-collapse').collapse('hide');
+      }
+    });
 
-		$('.introSectionScroll').click(function() {
-			if ($('.menuIcon').hasClass('open')) {
-				$('.menuIcon').removeClass('open');
-				$('.navbar-collapse').collapse('hide');
-			}
-		});
+    $('.introSectionScroll').click(function() {
+      if ($('.menuIcon').hasClass('open')) {
+        $('.menuIcon').removeClass('open');
+        $('.navbar-collapse').collapse('hide');
+      }
+    });
 
-		$('#scrollToTop').click(function() {
-			if ($('.menuIcon').hasClass('open')) {
-				$('.menuIcon').removeClass('open');
-				$('.navbar-collapse').collapse('hide');
-			}
-		});
+    $('.scrollToTop').click(function() {
+      if ($('.menuIcon').hasClass('open')) {
+        $('.menuIcon').removeClass('open');
+        $('.navbar-collapse').collapse('hide');
+      }
+    });
 
-		$('.navbar-toggler').click(function() {
-			$('.navbar-collapse').collapse('toggle');
-			$('.menuIcon').toggleClass('open');
-		});
+    $('.navbar-toggler').click(function() {
+      $('.navbar-collapse').collapse('toggle');
+      $('.menuIcon').toggleClass('open');
+    });
 
-		$('.navbar ul').click(function() {
-			if ($('.menuIcon').hasClass('open')) {
-				$('.menuIcon').removeClass('open');
-				$('.navbar-collapse').collapse('hide');
-			}
-		});
-	});
+    $('.navbar ul').click(function() {
+      if ($('.menuIcon').hasClass('open')) {
+        $('.menuIcon').removeClass('open');
+        $('.navbar-collapse').collapse('hide');
+      }
+    });
+  });
 });
